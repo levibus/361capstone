a27d0dd9a9f1b24a7e1b0b4bc4b79662c6e03efe
Levi Busching <levibusching@gmail.com>
2024-04-13 06:44:27 -0500

    random stuff

--- unitTests/obj/Debug/net7.0/unitTests.AssemblyInfo.cs
+++ unitTests/obj/Debug/net7.0/unitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("unitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94113135afdc788bcd05a1417d4395769bac8d8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d8daf404f14a0f4cc74fc527c8d3dde206206c")]
 [assembly: System.Reflection.AssemblyProductAttribute("unitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("unitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
