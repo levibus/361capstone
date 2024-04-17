8f85e08f8ecf224b0e3e0e15023877ef19417d9a
GupKodali <111927458+GupKodali@users.noreply.github.com>
2024-04-17 11:51:14 -0500

    CreateUser.cs

--- middleware/CreateUser.cs
+++ middleware/CreateUser.cs
@@ -13,15 +13,17 @@ namespace webpage
     public class CreateUserAccessor
     {  
         public Createuser(User user){
-            String conn= "Server=localhost;Port=1433;Database=master;Trusted_Connection=True;Data Source=.\\SQLEXPRESS";
-            try{
-                conn.Open();
-            }
-            catch (Exception ex) {
-                Console.WriteLine("Error - Could not open connection");
+            using (SqlConnection conn  = new SqlConnection()){
+                conn.ConnectionString = "Server=localhost;Port=1433;Database=master;Trusted_Connection=True;Data Source=.\\SQLEXPRESS";
+                try{
+                    conn.Open();
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Error - Could not open connection");
+                }
             }
 
-            sqlQuery = "INSERT INTO  Customer(firstName, lastName, username, password) values (@firstName, @lastName, @username, @password)"
+            string sqlQuery = "INSERT INTO  Customer(firstName, lastName, username, password) values (@firstName, @lastName, @username, @password)"
             using (SqlCommand cmd = new SqlCommand(sqlQuery, conn)){
                 
                 cmd.Parameters.Add("@fistName", user.fistName);
