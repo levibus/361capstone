817a9dcd4543fcf9041dc5e51464d694caf468fe
Levi Busching <levibusching@gmail.com>
2024-04-17 12:14:07 -0500

    createUser stuff

--- middleware/CreateUser.cs
+++ middleware/CreateUser.cs
@@ -6,40 +6,61 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 using Microsoft.AspNetCore.Identity;
+using System.Collections;
 
 
 namespace webpage
 {
     public class CreateUserAccessor
     {  
-        public Createuser(User user){
+       public int Createuser(User user){ //user class doesn't exist right now
+            object customerId = -1;
             using (SqlConnection conn  = new SqlConnection()){
-                conn.ConnectionString = "Server=localhost;Port=1433;Database=master;Trusted_Connection=True;Data Source=.\\SQLEXPRESS";
+                conn.ConnectionString = "Server=DESKTOP-QEQG0AR;Database=master;Trusted_Connection=True";
                 try{
                     conn.Open();
                 }
                 catch (Exception ex) {
                     Console.WriteLine("Error - Could not open connection");
                 }
-            }
 
-            string sqlQuery = "INSERT INTO  Customer(firstName, lastName, username, password) values (@firstName, @lastName, @username, @password)"
-            using (SqlCommand cmd = new SqlCommand(sqlQuery, conn)){
-                
-                cmd.Parameters.Add("@fistName", user.fistName);
-                cmd.Parameters.Add("@lastName", user.lastName);
-                cmd.Parameters.Add("@username", user.username);
-                cmd.Parameters.Add("@password", user.password);
+                string sqlInsert = "INSERT INTO  Customer(firstName, lastName, username, password) values (@firstName, @lastName, @username, @password)";
+                using (SqlCommand cmd = new SqlCommand(sqlInsert, conn)) {
 
-                //check if the amout was added
-                int rowsAffected = cmd.ExecuteNonQuery();
-                if(rowsAffected > 0) {
-                    Console.WriteLine("Rows Added");
+                    cmd.Parameters.Add("@fistName", user.fistName);
+                    cmd.Parameters.Add("@lastName", user.lastName);
+                    cmd.Parameters.Add("@username", user.username);
+                    cmd.Parameters.Add("@password", user.password);
+
+                    //check if the amout was added
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0) {
+                        Console.WriteLine("Rows Added");
+                    }
+                    else {
+                        Console.WriteLine("No Rows Added");
+                    }
+
+                    string sqlGetCustomerId = "SELECT customerId FROM Customer WHERE username = @username AND password = @password";
+
+                    using (SqlCommand command = new SqlCommand(sqlGetCustomerId, conn))
+                    {
+                        command.Parameters.AddWithValue("@username", user.username); 
+                        command.Parameters.AddWithValue("@password", user.password);
+
+                        customerId = command.ExecuteScalar();
+                        if (customerId == null)
+                        {
+                            customerId = -1; // customer not found
+                        }
+
+                    }
+
+
                 }
-                else{
-                    MessageBox.Show ("No Rows Added");
-                }
+                conn.Close();
             }
+            return (int)customerId;
         }
     }
 }
--- middleware/obj/Debug/net7.0/361capstone.AssemblyInfo.cs
+++ middleware/obj/Debug/net7.0/361capstone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("361capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69766203244a14629171d3f1daa0c544da340805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f85e08f8ecf224b0e3e0e15023877ef19417d9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("361capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("361capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
